1333604e43e9956bed68c25d90df803f18da3358
Sam3570 <40573202+Sam3570@users.noreply.github.com>
2025-05-06 13:08:08 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_BxbJYoS69', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'chtsiddka7ijg2cd5v86lp09t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_458PGNLIw', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4nk0v202m2qq1kd0ca4n10tlns', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
